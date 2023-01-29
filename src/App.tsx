11540f6187a5d7135b527fae9c0ab612bed43585
yataji <yassinoubil3@yahoo.com>
2023-01-29 19:42:50 +0100

    size

--- src/App.tsx
+++ src/App.tsx
@@ -21,28 +21,28 @@ function App() {
     <div className="w-screem h-screen pt-5 bg-[#383638]">
       <h2 className=" font-bold text-3xl mb-5">Let's get started!</h2>
       <div className='bg-[#383638] flex justify-center'>
-      <div className='flex flex-col justify-center items-center mx-5 px-3 shadow-xl py-5 w-3/6 rounded-xl bg-[#1e1d1e]'>
-        <ExpenseItem
-          title={tet[1].title}
-          amount={tet[1].amount}
-          date={tet[1].date}
-          />
+        <div className='flex flex-col justify-center items-center mx-5 px-3 shadow-xl py-5 sm:w-11/12 w-2/3  rounded-xl bg-[#1e1d1e]'>
           <ExpenseItem
-          title={tet[0].title}
-          amount={tet[0].amount}
-          date={tet[0].date}
-          />
-        <ExpenseItem
-          title={tet[1].title}
-          amount={tet[1].amount}
-          date={tet[1].date}
-          />
-        <ExpenseItem
-          title={tet[1].title}
-          amount={tet[1].amount}
-          date={tet[1].date}
-          />
-      </div>
+            title={tet[1].title}
+            amount={tet[1].amount}
+            date={tet[1].date}
+            />
+            <ExpenseItem
+            title={tet[0].title}
+            amount={tet[0].amount}
+            date={tet[0].date}
+            />
+          <ExpenseItem
+            title={tet[1].title}
+            amount={tet[1].amount}
+            date={tet[1].date}
+            />
+          <ExpenseItem
+            title={tet[1].title}
+            amount={tet[1].amount}
+            date={tet[1].date}
+            />
+        </div>
       </div>
     </div>
   );
